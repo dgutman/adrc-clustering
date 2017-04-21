e3383777f7318f80eae17dc8a93720867cfffa64
Mohammed Khalilia <mohammed.khalilia@gmail.com>
2017-04-21 15:29:53 -0400

    added config file and command line arguments

--- libs/clustering.py
+++ libs/clustering.py
@@ -30,23 +30,23 @@ def dunn_index(X, cluster_labels):
 	di = np.min(inter_clust_dist) / np.max(intra_clust_dist)
 	return di
 
-def sum_squared_error(X, centers, labels):
+def sum_squared_error(X, centers, cluster_labels):
 	""" sum_squared_error()
 	Compute the sum of sequared error. This is the sum of distances
 	between all data points in a cluster and the corresponding cluster
 	center.
 	"""
 	sse = 0
-	ulabels = set(list(labels))
+	labels = set(list(cluster_labels))
 
-	for label in ulabels:
+	for label in labels:
 		c = centers[label]
-		idx = np.where(labels == label)
+		idx = np.where(cluster_labels == label)
 		sse += sum([np.linalg.norm(c-x) for x in X[idx,:]])
 
 	return sse
 
-def reordered_dist_matrix(X, labels):
+def reordered_dist_matrix(X, cluster_labels, output):
 	""" reordered_dist_matrix()
 	Given the feature vector matrix X and the cluster labels, compute 
 	the distance between all data points then reorder the distance matrix
@@ -54,10 +54,10 @@ def reordered_dist_matrix(X, labels):
 	are adjacent.
 	"""
 	index = np.empty(shape=(0,0))
-	ulabels = list(set(labels))
+	labels = list(set(cluster_labels))
 
-	for label in ulabels:
-		idx = np.where(labels == label)[0]
+	for label in labels:
+		idx = np.where(cluster_labels == label)[0]
 		index = np.append(index, idx)
 
 	D = squareform(pdist(X, 'euclidean'))
@@ -65,21 +65,21 @@ def reordered_dist_matrix(X, labels):
 	reordered_d = D[index,:][:,index]
 	plt.imshow(reordered_d, cmap='Greys_r')
 	plt.colorbar()
-	plt.savefig('results/clusters.png')
+	plt.savefig(output)
 	plt.clf()
 
-def cluster_centroids(X, patients, centers, labels):
+def cluster_centroids(X, patients, centers, cluster_labels):
 	""" cluster_centroids()
 	Given feature vector matrix X, patients, cluster centers and label
 	find the patients most representitive of each cluster, the centroid.
 	A centroid is the patient closest to the cluster center.
 	"""
 	centroids = {}
-	ulabels = list(set(labels))
+	labels = list(set(cluster_labels))
 
-	for label in labels:
+	for label in cluster_labels:
 		centroids[label] = []
-		idx = np.where(labels == label)[0]
+		idx = np.where(cluster_labels == label)[0]
 		clust_pats = [patients[i] for i in idx]
 		center_patient_dist = euclidean_distances(centers[label].reshape(1,-1), X[idx,:])
 		idx = np.argsort(center_patient_dist)[0]
@@ -87,17 +87,17 @@ def cluster_centroids(X, patients, centers, labels):
 
 	return centroids
 
-def cluster_words(patients, labels):
+def cluster_words(patients, cluster_labels):
 	""" cluster_words()
 	Given patients and their corresponding cluster labels compute
 	the frequency of each word in that cluster and return a dictionary 
 	of WORD:FREQUENCY
 	"""
-	ulabels = list(set(labels))
+	labels = list(set(cluster_labels))
 	clust = []
 
-	for label in ulabels:
-		idx = np.where(labels == label)[0]
+	for label in labels:
+		idx = np.where(cluster_labels == label)[0]
 		words = list(itertools.chain(*[set(patients[i].words) for i in idx]))
 		c = Counter(words)
 
@@ -108,17 +108,17 @@ def cluster_words(patients, labels):
 
 	return clust
 
-def cluster_ngrams(patients, labels):
+def cluster_ngrams(patients, cluster_labels):
 	""" cluster_ngrams()
 	Given patients and their corresponding cluster labels compute 
 	ngrams (n=2). This function is not used anywhere for now
 	"""
-	ulabels = list(set(labels))
+	labels = list(set(cluster_labels))
 	clust = []
 
-	for label in ulabels:
+	for label in labels:
 		grams2 = []
-		idx = np.where(labels == label)[0]
+		idx = np.where(cluster_labels == label)[0]
 		for i in idx:
 			for gram in ngrams(patients[i].words, 2):
 				grams2.append(gram)
@@ -131,19 +131,19 @@ def cluster_ngrams(patients, labels):
 
 	return clust
 
-def cluster_word_importance(patients, labels):
+def cluster_word_importance(patients, cluster_labels, output):
 	""" cluster_word_importance()
 	Given patients and their corresponding cluster labels let us compute
 	the TF-IDF for each word in each cluster. See tfidf.py for more details.
 	The results are saved to CSV file.
 	"""
-	ulabels = list(set(labels))
+	labels = list(set(cluster_labels))
 	documents = []
 	results =[]
 	clust_size = []
 
-	for label in ulabels:
-		idx = np.where(labels == label)[0]
+	for label in labels:
+		idx = np.where(cluster_labels == label)[0]
 		clust_words = list(itertools.chain(*[patients[i].words for i in idx]))
 		documents.append(" ".join(clust_words))
 		clust_size.append(float(len(idx)))
@@ -151,15 +151,15 @@ def cluster_word_importance(patients, labels):
 	features, tfidf, tf = get_tfidf(documents)
 	feature = np.array(features)
 
-	for k in ulabels:
+	for k in labels:
 		idx = np.argsort(tfidf[k][::-1][0:30])
 		results.append(np.array(features)[idx])
 		results.append( (tf[k][idx] / clust_size[k]) * 100 )
 
 	print np.stack(results, axis=1)
-	np.savetxt("results/tfidf.csv", np.stack(results, axis=1), fmt="%s,%s,%s,%s,%s,%s,%s,%s")
+	np.savetxt(output, np.stack(results, axis=1), fmt="%s"*len(labels)*2)
 
-def plot_dunn_index(di, n_clusters):
+def plot_dunn_index(di, n_clusters, output):
 	""" plot_dunn_index()
 	Given dunn index for n clusters generate a graph
 	and save it
@@ -168,10 +168,10 @@ def plot_dunn_index(di, n_clusters):
 	plt.xlabel("Number of Clusters")
 	plt.ylabel("Dunn's Index")
 	plt.xticks(range(0,len(n_clusters)), n_clusters)
-	plt.savefig('results/dunn_index.png')
+	plt.savefig(output)
 	plt.clf()
 
-def plot_sse(sse, n_clusters):
+def plot_sse(sse, n_clusters, output):
 	""" plot_sse()
 	Given sum of squared error for n clusters generate a graph
 	and save it
@@ -180,5 +180,5 @@ def plot_sse(sse, n_clusters):
 	plt.xlabel("Number of Clusters")
 	plt.ylabel("Sum of Squared Error")
 	plt.xticks(range(0,len(n_clusters)), n_clusters)
-	plt.savefig('results/sse.png')
+	plt.savefig(output)
 	plt.clf()
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #!/home/mkhali8/anaconda2/envs/adrc-clustering/bin/python
 
+import os
+import sys
+import argparse
 import numpy as np
 from sklearn.cluster import KMeans
 import matplotlib as mpl
@@ -7,70 +10,99 @@ mpl.use('Agg')
 import matplotlib.pyplot as plt
 from libs.features import *
 from libs.clustering import *
+from config import *
 
-# Read patient data from CSV file and convert into Dict
-# Aggregate verbal fluency tests and compute patient 
-# graph and its features
-input_file = "/home/mkhali8/dev/adrc-clustering/data/WordFluencyMultiTest.csv"
-patients = read_data_from_file(input_file)
-patients = aggregate_fluency_tests(patients)
-patients = word_graph(patients)
+# Define functions
+def compute_sse(X): 
+	""" compute_sse
+	Compute the sum of squared error for n clusters
+	"""
+	sse = []
+	n_clusters = range(2,15)
 
-# Generate feature vector matrix using graph features
-n = len(patients)
-m = len(patients[0].features) + 1
-A = np.zeros(shape=(n,m))
-features = patients[0].features.keys() + ["errors"]
+	for n in n_clusters:
+		km = KMeans(n_clusters=n)
+		clustering = km.fit(X)
+		sse.append(sum_squared_error(A, clustering.cluster_centers_, km.labels_))
 
-for patient in patients:
-	A[patient.index] = patient.features.values() + [len(patient.errors)]
+	plot_sse(sse, n_clusters, os.path.join(results_dir, 'sse.png'))
 
-# Compute the sum of squared error (SSE)
-# and the dunn index (DI) for meausre cluster quality
-sse = []
-di = []
-n_clusters = range(2,15)
+def compute_di(X): 
+	""" compute_di
+	Compute dunn index for n clusters
+	"""
+	di = []
+	n_clusters = range(2,15)
 
-for k in n_clusters:
-	km = KMeans(n_clusters=k)
-	clustering = km.fit(A)
+	for n in n_clusters:
+		km = KMeans(n_clusters=n)
+		clustering = km.fit(X)
+		sse.append(sum_squared_error(A, clustering.cluster_centers_, km.labels_))
+
+	plot_dunn_index(di, n_clusters, os.path.join(results_dir, 'dunn_index.png'))
+
+if __name__ == "__main__":
+	# Parse command line arguments
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-k", "--n_clusters", default=4, type=int, help="number of clusters")
+	parser.add_argument("-i", "--input", default=INPUT_FILE, help="Input file (full path)")
+	parser.add_argument("-o", "--output", default=OUTPUT_DIR, help="Output directory")
+	parser.add_argument("-sse", "--squared_error", help="Compute sum of squared error", action="store_true")
+	parser.add_argument("-di", "--dunn_index", help="Compute dunn index", action="store_true")
+	args = parser.parse_args()
+
+	# Initialize some variables
+	results_dir = os.path.join(args.output, "k"+str(args.n_clusters))							# output dir
+	
+	# Read patient data from CSV file and convert into Dict
+	# Aggregate verbal fluency tests and compute patient 
+	# graph and its features
+	patients = read_data_from_file(args.input)
+	patients = aggregate_fluency_tests(patients)
+	patients = word_graph(patients)
+
+	# Generate feature vector matrix using graph features
+	n = len(patients)																			# number of patients
+	m = len(patients[0].features) + 1 															# number of features
+	X = np.zeros(shape=(n,m))																	# Feature matrix
+	features = patients[0].features.keys() + ["errors"]
+
+	for patient in patients:
+		X[patient.index] = patient.features.values() + [len(patient.errors)]
+
+	if args.squared_error:
+		compute_sse(X)
+	if args.dunn_index:
+		compute_di(X)
+
+	# Run k-means for some k clusters
+	km = KMeans(n_clusters=args.n_clusters)
+	clustering = km.fit(X)
 	centers = clustering.cluster_centers_
 	labels = list(set(km.labels_))
-	index_order = np.empty(shape=(0,0))
 
-	di.append(dunn_index(A, km.labels_))
-	sse.append(sum_squared_error(A, centers, km.labels_))
+	# Given the matrix A and cluster labels
+	# Compute distance between centers and patients
+	# Reorder the matrix, then save the image
+	reordered_dist_matrix(X, km.labels_, os.path.join(results_dir, 'reordered_sim_matrix.png'))
 
-plot_dunn_index(di, n_clusters)
-plot_sse(sse, n_clusters)
+	# Save cluster centers and TF-IDF values for words in each cluster
+	clust_centroids = cluster_centroids(X, patients, centers, km.labels_)
+	cluster_word_importance(patients, km.labels_, os.path.join(results_dir, 'tfidf.csv'))
 
-# Run k-means for some k clusters
-km = KMeans(n_clusters=4)
-clustering = km.fit(A)
-centers = clustering.cluster_centers_
-labels = list(set(km.labels_))
+	# Find representitive patients for each cluster and save
+	# their verbal fluency graph
+	for label, centroids in clust_centroids.iteritems():
+		for centroid in centroids:
+			nx.draw_spectral(centroid.graph)
+			output = os.path.join(results_dir, 'cluster_%d_%d.png' % (label, centroid.index))
+			plt.savefig(output)
+			plt.clf() 
 
-# Given the matrix A and cluster labels
-# Compute distance between centers and patients
-# Reorder the matrix, then save the image
-reordered_dist_matrix(A, km.labels_)
+	# Write cluster center to file
+	with open(os.path.join(results_dir, "cluster_centers.csv"), "w") as fh:
+		w = csv.writer(fh)
+		w.writerow(features)
 
-# Save cluster centers and TF-IDF values for words in each cluster
-clust_centroids = cluster_centroids(A, patients, centers, km.labels_)
-cluster_word_importance(patients, km.labels_)
-
-# Find representitive patients for each cluster and save
-# their verbal fluency graph
-for label, centroids in clust_centroids.iteritems():
-	for centroid in centroids:
-		nx.draw_spectral(centroid.graph)
-		plt.savefig('results/cluster_%d_%d.png' % (label, centroid.index))
-		plt.clf() 
-
-# Write cluster center to file
-with open("results/cluster_centers.csv", "w") as fh:
-	w = csv.writer(fh)
-	w.writerow(features)
-
-	for center in centers:
-		w.writerow(center)
+		for center in centers:
+			w.writerow(center)
